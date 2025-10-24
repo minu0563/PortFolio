1629e2a791e8103ea2bd79cc0aff8464b3738b66
Minu0563 <dgawg4977@gmail.com>
2025-10-24 23:44:23 +0900

    commit 2

--- app/aboutpage/page.tsx
+++ app/aboutpage/page.tsx
@@ -1,17 +1,23 @@
 import Link from "next/link";
+import AnimatedSection from '../AnimatedSection';
+
 
 export default function Home() {
     return (
-        <div className="flex justify-center items-center mt-5">
-            <Link href="/aboutpage/KwakDoYoung" className="px-6 py-3 bg-gradient-to-br from-blue-900 via-sky-200 to-cyan-500  text-orange-600 rounded-lg">
-                KwakDoYoung
-            </Link>
-            <Link href="/aboutpage/ParkJuHyun" className="px-6 py-3 bg-gradient-to-br from-violet-500 via-pink-950 to-zinc-500 text-cyan-300 rounded-lg ml-4">
-                ParkJuHyun
-            </Link>
-            <Link href="/aboutpage/user3" className="px-6 py-3 bg-gradient-to-br from-green-500 via-cyan-700 to-yellow-400 text-purple-700 rounded-lg ml-4">
-                user3
-            </Link>
+        <div className="flex justify-center items-center flex-col">
+            <p>PortFolio</p>
+            <div className="flex justify-center items-center w-full h-screen">
+                <AnimatedSection>
+                    <Link href="/aboutpage/KwakDoYoung" className="px-6 py-3 rounded-lg border-2 border-gray-400 bg-clip-text text-transparent bg-gradient-to-r from-red-400 to-white font-medium">
+                        Kwak Do Young
+                    </Link>
+                </AnimatedSection>  
+                <AnimatedSection delay={100}>
+                    <Link href="/aboutpage/ParkJuHyun" className="px-6 py-3 rounded-lg border-2 border-gray-400 bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-white font-medium">
+                        Park Ju Hyun
+                    </Link>
+                </AnimatedSection>
+            </div>
         </div>
     );
 }
