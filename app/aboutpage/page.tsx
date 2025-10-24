36e86adfc32c1d745caa097ecc186f44f65b4645
Minu0563 <dgawg4977@gmail.com>
2025-10-24 23:52:57 +0900

    commit testss

--- app/aboutpage/page.tsx
+++ app/aboutpage/page.tsx
@@ -5,7 +5,7 @@ import AnimatedSection from '../AnimatedSection';
 export default function Home() {
     return (
         <div className="flex justify-center items-center flex-col">
-            <p>PortFolio</p>
+            <p>PortFolios</p>
             <div className="flex justify-center items-center w-full h-screen">
                 <AnimatedSection>
                     <Link href="/aboutpage/KwakDoYoung" className="px-6 py-3 rounded-lg border-2 border-gray-400 bg-clip-text text-transparent bg-gradient-to-r from-red-400 to-white font-medium">
