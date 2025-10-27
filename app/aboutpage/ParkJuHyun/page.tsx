f6773f999f27c24aeddc4a1b10bd593c4806ad89
minu0563 <dgwag4977@gmail.com>
2025-10-27 09:14:00 +0900

    this is our first commit

--- app/aboutpage/ParkJuHyun/page.tsx
+++ app/aboutpage/ParkJuHyun/page.tsx
@@ -10,16 +10,16 @@ export default function Home() {
             <hr />
             <div className="justify-center items-center text-center">
                 <AnimatedSection delay={100} className='min-h-screen'>
-                    <h3 className="mt-5"> elcome to my portfolio page!</h3>
-                    <Image src="/imsiimg.png" alt="" width={600} height={400} className="rounded-full mx-auto mt-5" />
+                    <h3 className="mt-5"> Welcome to my portfolio page!</h3>
+                    <Image src="/imsiimage.png" alt="" width={600} height={400} className="rounded-full mx-auto mt-5 " />
                 </AnimatedSection>
                 <AnimatedSection delay={100} className='min-h-screen'>
                     <h2 className="mt-5"> This website want to create together with a friend.</h2>
-                    <Image src="/imsiimg.png" alt="" width={600} height={400} className="rounded-full mx-auto mt-5" />
+                    <Image src="/imsiimage.png" alt="" width={600} height={400} className="rounded-full mx-auto mt-5" />
                 </AnimatedSection>
                 <AnimatedSection delay={100} className='min-h-screen'>
                     <h2 className="mt-5"> I`ve worked on developing several <strong>extensions</strong> and <strong>websites</strong>, which you can explore here. </h2>
-                    <Image src="/imsiimg.png" alt="" width={600} height={400} className="rounded-full mx-auto mt-5" />
+                    <Image src="/imsiimage.png" alt="" width={600} height={400} className="rounded-full mx-auto mt-5" />
                 </AnimatedSection>
             </div>
         </div>
